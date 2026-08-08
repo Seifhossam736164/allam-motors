19132e4a4fad11332952bf81a4842d51678bbbfd
Seifhossam736164 <oneday.2.8267@gmail.com>
2026-08-08 03:53:04 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const products = [
+
+{
+    id: 1,
+    name: "Yamaha R15 V4",
+    price: 195000,
+    category: "motorcycles",
+    brand: "Yamaha",
+
+    images: [
+        "images/r15-1.jpg",
+        "images/r15-2.jpg",
+        "images/r15-3.jpg",
+        "images/r15-4.jpg"
+    ],
+
+    description: "موتور رياضي 155cc مناسب للاستخدام اليومي والسفر.",
+
+    specs: {
+        model: "2026",
+        engine: "155 CC",
+        gearbox: "6 سرعات",
+        color: "أزرق",
+        condition: "جديد"
+    }
+},
+
+{
+    id: 2,
+    name: "Honda CBR 250R",
+    price: 210000,
+    category: "motorcycles",
+    brand: "Honda",
+
+    images: [
+        "images/cbr-1.jpg",
+        "images/cbr-2.jpg",
+        "images/cbr-3.jpg"
+    ],
+
+    description: "موتور رياضي قوي واقتصادي.",
+
+    specs: {
+        model: "2026",
+        engine: "250 CC",
+        gearbox: "6 سرعات",
+        color: "أحمر",
+        condition: "جديد"
+    }
+}
+
+];
+
+
